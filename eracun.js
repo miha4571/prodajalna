67475d854f255304def13661ce5afa519c7a343e
miha <mk4571@student.uni-lj.si>
2016-05-08 21:32:04 +0000

    Zaključek dela na III. nalogi

--- eracun.js
+++ eracun.js
@@ -133,7 +133,7 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      console.log(vrstice);
+      callback(vrstice);
     })
 }
 
@@ -142,13 +142,25 @@ var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
             WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
     function(napaka, vrstice) {
-      console.log(vrstice);
+      callback(vrstice);
     })
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
+  var form = new formidable.IncomingForm();
+  form.parse(zahteva, function(napaka, polja, datoteke) {
+    pesmiIzRacuna(polja.seznamRacunov ,function (pesmi) {
+      strankaIzRacuna(polja.seznamRacunov, function (stranka) {
+        odgovor.setHeader("content-type", "text/xml");
+        odgovor.render("eslog", {
+          vizualiziraj : true,
+          stranka : stranka,
+          postavkeRacuna : pesmi
+        })
+      })
+    })
+  })
 })
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
